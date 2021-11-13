2320c400a278b057e6033ea4c752d8b99c668754
Filipe Silva <filipeqs@outlook.com>
2021-11-13 14:23:42 -0500

    exploring properties and methods

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
     templateUrl: './user.component.html',
     styleUrls: ['./user.component.css'],
 })
-export class UserComponent {}
+export class UserComponent {
+    // Properties
+    firstName = 'John';
+    lastName = 'Doe';
+    age = 30;
+    address = {
+        street: '50 Main st',
+        city: 'Boston',
+        state: 'MS',
+    };
+
+    // Methods
+    constructor() {}
+}
