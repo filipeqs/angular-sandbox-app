3be381820a366b9122c05845e3a107ae4ddbbbac
Filipe Silva <filipeqs@outlook.com>
2021-11-13 19:14:50 -0500

    users component and turn strict mode off

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/User';
 
 @Component({
@@ -6,12 +6,14 @@ import { User } from 'src/app/models/User';
     templateUrl: './user.component.html',
     styleUrls: ['./user.component.css'],
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
     // Properties
     user: User;
 
     // Methods
-    constructor() {
+    constructor() {}
+
+    ngOnInit() {
         this.user = {
             firstName: 'John',
             lastName: 'Doe',
