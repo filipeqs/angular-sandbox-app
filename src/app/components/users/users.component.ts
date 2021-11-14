9b616fee5eb672407adc6a6df113d9a04c939604
Filipe Silva <filipeqs@outlook.com>
2021-11-13 19:45:43 -0500

    displaying users with ng for

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
 
 @Component({
     selector: 'app-users',
@@ -6,7 +7,56 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
+    users: User[];
+
     constructor() {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.users = [
+            {
+                firstName: 'John',
+                lastName: 'Doe',
+                age: 30,
+                address: {
+                    street: '50 Main st',
+                    city: 'Boston',
+                    state: 'MA',
+                },
+            },
+            {
+                firstName: 'Kevin',
+                lastName: 'Johnson',
+                age: 34,
+                address: {
+                    street: '20 School st',
+                    city: 'Lynn',
+                    state: 'MA',
+                },
+            },
+            {
+                firstName: 'Karen',
+                lastName: 'Williams',
+                age: 26,
+                address: {
+                    street: '55 Mill st',
+                    city: 'Miami',
+                    state: 'FL',
+                },
+            },
+        ];
+        this.addUser({
+            firstName: 'David',
+            lastName: 'Jackson',
+            age: 44,
+            address: {
+                street: '12 Wake st',
+                city: 'Miami',
+                state: 'FL',
+            },
+        });
+    }
+
+    addUser(user: User) {
+        this.users.push(user);
+    }
 }
