a11291b4bb92cf01680db32219032981359304ca
Filipe Silva <filipeqs@outlook.com>
2021-11-17 23:28:13 -0500

    fetch posts from jsonplacehoder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
@@ -7,11 +8,13 @@ import { UsersComponent } from './components/users/users.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { UserService } from './services/user.service';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostService } from './services/post.service';
 
 @NgModule({
-    declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent],
-    imports: [BrowserModule, FormsModule],
-    providers: [UserService],
+    declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent, PostsComponent],
+    imports: [BrowserModule, FormsModule, HttpClientModule],
+    providers: [UserService, PostService],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
