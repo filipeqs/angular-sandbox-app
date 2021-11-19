b3636497d6f5c63ee64c34fdb754c65fc1e5c14c
Filipe Silva <filipeqs@outlook.com>
2021-11-18 20:32:59 -0500

    add new post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { FormsModule } from '@angular/forms';
 import { UserService } from './services/user.service';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostService } from './services/post.service';
+import { PostFormComponent } from './components/post-form/post-form.component';
 
 @NgModule({
-    declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent, PostsComponent],
+    declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent, PostsComponent, PostFormComponent],
     imports: [BrowserModule, FormsModule, HttpClientModule],
     providers: [UserService, PostService],
     bootstrap: [AppComponent],
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,7 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { Post } from '../models/Post';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
 
 @Injectable({
     providedIn: 'root',
@@ -13,5 +18,9 @@ export class PostService {
 
     getPosts(): Observable<Post[]> {
         return this.http.get<Post[]>(this.postsUrl);
+    }
+
+    savePost(post: Post): Observable<Post> {
+        return this.http.post<Post>(this.postsUrl, post, httpOptions);
     }
 }
