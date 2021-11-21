39c9cd537fe2627e8560fbe72b6def6c9796a3b4
Filipe Silva <filipeqs@outlook.com>
2021-11-21 10:04:16 -0500

    routes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,12 @@ import { UserService } from './services/user.service';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostService } from './services/post.service';
 import { PostFormComponent } from './components/post-form/post-form.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-    declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent, PostsComponent, PostFormComponent],
-    imports: [BrowserModule, FormsModule, HttpClientModule],
+    declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent, PostsComponent, PostFormComponent, HomeComponent],
+    imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
     providers: [UserService, PostService],
     bootstrap: [AppComponent],
 })
